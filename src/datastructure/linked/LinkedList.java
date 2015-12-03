8549f1423600754ca0ce4708a2fd48d8d058a92c
Vasil Peevski <vassilpeevsky@gmail.com>
2015-12-03 07:01:57 +0200

    Linked list implementations started

--- src/datastructure/linked/LinkedList.java
+++ src/datastructure/linked/LinkedList.java
@@ -4,55 +4,74 @@ import datastructure.List;
 
 public class LinkedList<T> implements List<T> {
 
-  private Node<T> _fisrt;
+	private Node<T> _fisrt;
 
-  private Node<T> _last;
+	private Node<T> _last;
 
-  @Override
-  public boolean isEmpty() {
-    return _fisrt == null;
-  }
+	@Override
+	public boolean isEmpty() {
+		return _fisrt == null;
+	}
 
-  @Override
-  public int lenght() {
-    // TODO Auto-generated method stub
-    return 0;
-  }
+	@Override
+	public int lenght() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-  @Override
-  public void add(T value) {
-    if (isEmpty()) {
-      Node<T> newNode = new Node<T>(null, value, null);
-      _fisrt = _last = newNode;
-    } else {
-      Node<T> newNode = new Node<T>(_last, value, null);
-      _last = newNode;
-    }
+	@Override
+	public void add(T value) {
+		if (value == null) {
+			throw new NullPointerException("Can not add null to List");
+		}
+		if (isEmpty()) {
+			Node<T> newNode = new Node<T>(null, value, null);
+			_fisrt = _last = newNode;
+		} else {
+			Node<T> newNode = new Node<T>(_last, value, null);
+			_last = newNode;
+		}
 
-  }
+	}
 
-  @Override
-  public void add(int index, T value) {
-    // TODO Auto-generated method stub
+	@Override
+	public void add(int index, T value) {
+		if (0 > index || index > lenght() - 1) {
+			throw new IndexOutOfBoundsException("Index: " + index
+					+ " is out of range for List of lenght: " + lenght());
+		}
 
-  }
+		if (value == null) {
+			throw new NullPointerException("Can not add null to List");
+		}
+		
+		if(isEmpty()) {
+			Node<T> newNode = new Node<T>(null, value, null);
+			_fisrt = _last = newNode;
+		} else {
+//			Node<T> newNode = new Node<T>(node(index - 1), value, node(index + 1));
+//			_last = newNode;
+		}
+		
 
-  @Override
-  public T get(int index) {
-    T result = _fisrt.value;
-    return null;
-  }
+	}
 
-  private static class Node<E> {
-    E       value;
-    Node<E> next;
-    Node<E> prev;
+	@Override
+	public T get(int index) {
+		T result = _fisrt.value;
+		return null;
+	}
 
-    Node(Node<E> prev, E element, Node<E> next) {
-      this.value = element;
-      this.next = next;
-      this.prev = prev;
-    }
-  }
+	private static class Node<E> {
+		E value;
+		Node<E> next;
+		Node<E> prev;
+
+		Node(Node<E> prev, E element, Node<E> next) {
+			this.value = element;
+			this.next = next;
+			this.prev = prev;
+		}
+	}
 
 }

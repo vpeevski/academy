b2587276afe8ec36fa932137525828d712b718a9
Vasil Peevski <vassilpeevsky@gmail.com>
2016-04-13 15:55:46 +0300

    Build matrix spiral

---
+++ src/algo/matrix/BuildMatrixSpiral.java
@@ -0,0 +1,50 @@
+package algo.matrix;
+
+import java.util.Scanner;
+
+import util.MatrixUtil;
+
+public class BuildMatrixSpiral {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter columns number : ");
+		int columns = scanner.nextInt();
+		System.out.println("Enter rows number : ");
+		int rows = scanner.nextInt();
+		
+		int[][] matrix = new int [rows][columns];
+		int number = 1;
+		
+		int start = 0;
+		int endColumn = columns - 1;
+		int endRow = rows - 1;
+		
+		for (int r = 0; r <= rows / 2 && start <= endColumn && start <= endRow; r++) {
+			for (int c = start; c <= endColumn; c++) {
+				matrix[r][c] = number++;
+			}
+			
+			for (int j = start + 1; j <= endRow; j++) {
+				matrix[j][endColumn] = number++;
+			}
+			
+			for (int c = endColumn - 1; c >= start; c--) {
+				matrix[endRow][c] = number++;
+			}
+			
+			for (int j = endRow - 1; j > start; j--) {
+				matrix[j][start] = number++;
+			}
+			
+			
+			start++;
+			endColumn--;
+			endRow--;
+		}
+		
+		MatrixUtil.print(matrix);
+		
+		
+	}
+}

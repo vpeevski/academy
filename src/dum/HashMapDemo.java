5ee5330563c21b0ef72e81615b2e450e6daaac0c
Vasil <vassilpeevsky@gmail.com>
2016-09-28 15:55:02 +0300

    HashMapDemo

---
+++ src/dum/HashMapDemo.java
@@ -0,0 +1,44 @@
+package dum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+  public static void main(String[] args) {
+    Map personHobies = new HashMap();
+    Person ivan = new Person(34, "Ivan");
+    Person dragan = new Person(35, "Dragan");
+    personHobies.put(ivan, "Swimming");
+    personHobies.put(dragan, "Reading books");
+    System.out.println(personHobies);
+  }
+
+  private static class Person {
+    private int    _age = 0;
+    private String _name;
+
+    public Person(int age, String name) {
+      _age = age;
+      _name = name;
+    }
+    
+    @Override
+    public int hashCode() {
+      return 0;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+      return _age == ((Person) obj)._age;
+    }
+    
+    @Override
+    public String toString() {
+      return _name + " age " + _age;
+    }
+  }
+  
+  
+
+}

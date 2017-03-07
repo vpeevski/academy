e8c0903015a1ec91103a64f23eb97c6e273320a5
vpeevski <vassilpeevsky@gmail.com>
2017-03-07 14:06:54 +0200

    Change max size to 30 because ...

--- src/datastructure/ArrayList.java
+++ src/datastructure/ArrayList.java
@@ -5,7 +5,7 @@ import java.util.NoSuchElementException;
 
 public class ArrayList<T> extends AbstractList<T> implements List<T> {
 
-  private static final int  MIN_SIZE = 5;
+  private static final int  MIN_SIZE = 30;
 
   private ArrayListSized<T> _innerSizedList = new ArrayListSized<T>(MIN_SIZE);
 
